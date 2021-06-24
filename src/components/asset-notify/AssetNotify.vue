383857514f15fc912ad56e09009478073a039001
zchong <zhch_hong@163.com>
2021-06-24 13:53:59 +0800

    fix: 资产数量转化

--- src/components/asset-notify/AssetNotify.vue
+++ src/components/asset-notify/AssetNotify.vue
@@ -76,21 +76,16 @@ export default defineComponent({
      * @param {需要转化的数} num
      * @param {需要保留的小数位数} point
      */
-    tranNumber(num: number, point: number) {
+    tranNumber(num: number, point?: number) {
       let numStr = num.toString();
-      // 十万以内直接返回
-      if (numStr.length < 6) {
+      if (numStr.length < 5) {
         return numStr;
-      }
-      //大于8位数是亿
-      else if (numStr.length > 8) {
-        let decimal = numStr.substring(numStr.length - 8, numStr.length - 8 + point);
-        return parseFloat(parseInt((num / 100000000).toString()) + '.' + decimal) + '亿';
-      }
-      //大于6位数是十万 (以10W分割 10W以下全部显示)
-      else if (numStr.length > 5) {
-        let decimal = numStr.substring(numStr.length - 4, numStr.length - 4 + point);
-        return parseFloat(parseInt((num / 10000).toString()) + '.' + decimal) + '万';
+      } else if (numStr.length < 9) {
+        if (num % 10000 === 0) return num / 10000 + '万';
+        return (num / 10000).toFixed(point || 2) + '万';
+      } else {
+        if (num % 100000000 === 0) return num / 10000 + '亿';
+        return (num / 100000000).toFixed(point || 2) + '亿';
       }
     },
 
